5179bfddbafdc62b3507da003f11e7d94d19edb1
brokestar233 <3765589194@qq.com>
2025-07-26 21:25:00 +0800

    module: update custom_module_blacklist
    Signed-off-by: brokestar233 <3765589194@qq.com>

--- kernel/module/main.c
+++ kernel/module/main.c
@@ -2388,10 +2388,11 @@ static char *custom_module_blacklist[] = {
     "lzo", "lzo_rle",
 #endif
 #if IS_BUILTIN(CONFIG_ZRAM)
-    "zram",
+	"oplus_bsp_hybridswap_zram",
+	"oplus_bsp_zram_opt",
 #endif
 #if IS_BUILTIN(CONFIG_ZSMALLOC)
-    "zsmalloc",
+    "oplus_bsp_zsmalloc",
 #endif
 #if IS_BUILTIN(CONFIG_TCP_CONG_BBR)
     "oplus_network_tuning",
