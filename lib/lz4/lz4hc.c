af8aca514bebb8bca3077e5643840f4d8386da76
brokestar233 <3765589194@qq.com>
2025-08-04 19:25:41 +0800

    Revert "lib/lz4: Remove redundant MIN/MAX macros to fix compilation error"
    This reverts commit da7cbb3fc254e89b54754aaf3af178a602cd454a.

--- lib/lz4/lz4hc.c
+++ lib/lz4/lz4hc.c
@@ -74,6 +74,10 @@ typedef enum { noDictCtx, usingDictCtxHc } dictCtx_directive;
 #define OPTIMAL_ML (int)((ML_MASK - 1) + MINMATCH)
 #define LZ4_OPT_NUM (1 << 12)
 
+/*===   Macros   ===*/
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
 /*===   Levels definition   ===*/
 typedef enum { lz4mid, lz4hc, lz4opt } lz4hc_strat_e;
 typedef struct {
