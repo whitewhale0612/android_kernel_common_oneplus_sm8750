3c4f933f5e47acc63ea4c4fdf04fd99db8b26205
brokestar233 <3765589194@qq.com>
2025-08-10 19:54:34 +0800

    PM / wakeup：避免在 pm_system_wakeup（） 中尝试过多的 s2idle 唤醒 在pm_abort_suspend递增后调用一次 s2idle_wake（） 就是 这是从 s2idle 中唤醒所必需的。避免多次不必要的尝试 仅在 pm_abort_suspend 命中 1 时唤醒，从 s2idle 唤醒。 s2idle 机器已经提供了使 这个保险箱。 在suspend中修补了相关属性

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -575,6 +575,9 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 	/* Increment the counter of events in progress. */
 	cec = atomic_inc_return(&combined_event_count);
 
+	if (events_check_enabled)
+		suspend_abort_fs_sync();
+
 	trace_wakeup_source_activate(ws->name, cec);
 }
 
