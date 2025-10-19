e49e3881ccbce69fac262b44eaca983e8fbe2fe7
whitewhale0612 <2136512929@qq.com>
2025-10-19 22:52:51 +0800

    ANDROID: vendor_hook: Added hook for memory reclaim tuning
    The following hooks are added for memory reclaim tuning:
    
    android_vh_mm_customize_lru_add_dst - We create private LRU lists for some
    folios used by specific processes, and these folios are managed separately.
    This hook is used to ensure that the aforementioned separately managed folios
    are also properly handled during split_huge_page.
    
    android_vh_mm_isolate_priv_lru - This hook is used to properly handle certain
    folios when they are reclaimed.
    
    android_vh_mm_customize_pgdat_balanced - When there are multiple zones, it is
    not always reasonable to perform indirect collection only when all zones do not
    meet the watermark. This hook allows vendors to customize pgdat_balanced to use
    their own strategy to determine whether to perform and stop indirect collection.
    
    android_vh_mm_customize_file_is_tiny - For a specific process, we want only
    anonymous pages to be reclaimed. This hook allows vendors to modify the value
    of file_is_tiny to affect the reclaim behavior.
    
    Bug: 431730501
    Change-Id: I860c2665088753a5c3b6d6dfeeb25a82aeefc8af
    Signed-off-by: Pengfei Li <pengfei.kernel@vivo.corp-partner.google.com>

--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -499,6 +499,9 @@ DECLARE_HOOK(android_vh_copy_page_to_user,
 DECLARE_HOOK(android_vh_copy_page_from_user,
 	TP_PROTO(struct page *page),
 	TP_ARGS(page));
+DECLARE_HOOK(android_vh_mm_customize_lru_add_dst,
+	TP_PROTO(struct lruvec *lruvec, struct folio *src, struct folio *dst, bool *added),
+	TP_ARGS(lruvec, src, dst, added));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
--- include/trace/hooks/vmscan.h
+++ include/trace/hooks/vmscan.h
@@ -110,6 +110,17 @@ DECLARE_HOOK(android_vh_direct_reclaim_begin,
 DECLARE_HOOK(android_vh_direct_reclaim_end,
 	TP_PROTO(int prio),
 	TP_ARGS(prio));
+DECLARE_HOOK(android_vh_mm_isolate_priv_lru,
+	TP_PROTO(unsigned long nr_to_scan, struct lruvec *lruvec, enum lru_list lru,
+		struct list_head *dst, int reclaim_idx, bool may_unmap,
+		unsigned long *nr_scanned, unsigned long *nr_taken),
+	TP_ARGS(nr_to_scan, lruvec, lru, dst, reclaim_idx, may_unmap, nr_scanned, nr_taken));
+DECLARE_HOOK(android_vh_mm_customize_pgdat_balanced,
+	TP_PROTO(int order, int highest_zoneidx, bool *balanced, bool *customized),
+	TP_ARGS(order, highest_zoneidx, balanced, customized));
+DECLARE_HOOK(android_vh_mm_customize_file_is_tiny,
+	TP_PROTO(unsigned int may_swap, int order, int highest_zoneidx, bool *file_is_tiny),
+	TP_ARGS(may_swap, order, highest_zoneidx, file_is_tiny));
 #endif /* _TRACE_HOOK_VMSCAN_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>
